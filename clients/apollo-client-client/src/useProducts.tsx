3e38ae82c1fee7d7fef05a885707cfcf4d3db4b2
Austin C Roos <austin.roos@wetransfer.com>
2021-02-10 23:52:18 +0100

    Show how to use multiple clients with Apollo

--- clients/apollo-client-client/src/useProducts.tsx
+++ clients/apollo-client-client/src/useProducts.tsx
@@ -1,4 +1,4 @@
-import { useQuery, gql } from '@apollo/client'
+import { useQuery, gql, ApolloClient, InMemoryCache } from '@apollo/client'
 import { ProductsQueryResponse } from './types'
 
 const QUERY = gql`
@@ -11,6 +11,11 @@ const QUERY = gql`
   }
 `
 
+const nuClient = new ApolloClient({
+  uri: 'http://localhost:3001/graphql',
+  cache: new InMemoryCache(),
+})
+
 export default function useProducts() {
-  return useQuery<ProductsQueryResponse, any>(QUERY)
+  return useQuery<ProductsQueryResponse, any>(QUERY, { client: nuClient })
 }
